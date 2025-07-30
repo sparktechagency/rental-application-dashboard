f1b3d039097d16df8e333804a492125822a1f53f
rakibislam2233 <rakib2020.tkg@gmail.com>
2025-07-30 09:04:09 +0600

    ADD

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ["jodit-react", "jodit"]
+    include: ["jodit-react", "jodit"],
   },
   server: {
-    host: "0.0.0.0",
+    host: true,
     port: 7002,
-    allowedHosts: [
-      "localhost",
-      "127.0.0.1",
-      "rakib7002.sobhoy.com" ,
-      "52.15.122.14"
-    ]
-  }
+  },
 });
