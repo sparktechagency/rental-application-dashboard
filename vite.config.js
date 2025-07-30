4fbb2fe5e63d6491fce4c7abb2ab539fd2ad6d41
rakibislam2233 <rakib2020.tkg@gmail.com>
2025-07-30 09:29:35 +0600

    ADd

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 7002,
+     allowedHosts: ["all"], 
+  },
+  preview: {
+    host: true,
+    port: 4173, 
+    allowedHosts: ["admin.kvrentals.us", "all"], 
   },
 });
